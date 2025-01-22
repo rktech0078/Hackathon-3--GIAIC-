be32c7ef73348e059f4872bf28aba3c05dc1fab6
rktech0078 <rafaykhan007852@gmail.com>
2025-01-22 17:36:55 +0500

    Middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -30,6 +30,17 @@
 // };
 
 
+// import { clerkMiddleware } from '@clerk/nextjs/server'
+
+// export default clerkMiddleware()
+
+// export const config = {
+//   matcher: [
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     '/(api|trpc)(.*)',
+//   ],
+// }
+
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware()
@@ -38,6 +49,7 @@ export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
+    '/studio',  // Sanity Studio ko match karne ke liye add kiya
   ],
 }
 
